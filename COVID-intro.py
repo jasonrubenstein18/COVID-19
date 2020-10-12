bfd2bd61cf8e8aae7586add6a5bf61600608f647
Jason Rubenstein <jasonr34@outlook.com>
2020-10-12 17:36:43 -0400

    Update COVID-intro.py

--- COVID-intro.py
+++ COVID-intro.py
@@ -11,76 +11,76 @@ import plotly_express
 path = '/Users/jasonrubenstein/Desktop/Python/covid/COVID-19/csse_covid_19_data/csse_covid_19_daily_reports_us/*.csv'
 covid_files = glob.glob(path)
 
-
-def get_csvs(files):
-    datas = pd.concat([
-        pd.read_csv(datas,
-                    dtype=str,
-                    error_bad_lines=False,
-                    delimiter=',')
-        for datas in files], axis=0)
-    df = datas.drop_duplicates(keep='first').reset_index(drop=True)
-    return df
-
-
-covid_data = get_csvs(covid_files)
-covid_data = covid_data[covid_data['Last_Update'].notna()]
-covid_data = covid_data.sort_values(by="Last_Update")
-covid_data = covid_data.fillna(0)
+class COVID():
+    def get_csvs(files):
+        datas = pd.concat([
+            pd.read_csv(datas,
+                        dtype=str,
+                        error_bad_lines=False,
+                        delimiter=',')
+            for datas in files], axis=0)
+        df = datas.drop_duplicates(keep='first').reset_index(drop=True)
+        return df
 
 
-# Adjust dtype as necessary
-def to_ints(df):
-    df['Last_Update'] = pd.to_datetime(df['Last_Update']).dt.date
-    df['Last_Update'] = pd.to_datetime(df['Last_Update'])
-    df.rename(columns={'Last_Update': 'Date'}, inplace=True)
-    df['Confirmed'] = df['Confirmed'].astype("int")
-    df['Deaths'] = df['Deaths'].astype("int")
-    # df['People_Tested'].str.replace('.', '').astype(int)
-    df['People_Tested'] = pd.to_numeric(df['People_Tested'])
-    # df['People_Tested'] = df['People_Tested'].astype("int")
-    df['People_Hospitalized'] = pd.to_numeric(df['People_Hospitalized'])
-    # df['People_Hospitalized'] = df['People_Hospitalized'].astype("int")
-    return df
+    covid_data = get_csvs(covid_files)
+    covid_data = covid_data[covid_data['Last_Update'].notna()]
+    covid_data = covid_data.sort_values(by="Last_Update")
+    covid_data = covid_data.fillna(0)
 
 
-def shift_data(df):
-    df['Prev_Cases'] = df.groupby("Province_State")["Confirmed"].shift(1)
-    df['Prev_Deaths'] = df.groupby("Province_State")["Deaths"].shift(1)
-    df['Prev_Tests'] = df.groupby("Province_State")["People_Tested"].shift(1)
-    df['Prev_Hospitalizations'] = df.groupby("Province_State")["People_Hospitalized"].shift(1)
-    df = df.fillna(0)
-    return df
+    # Adjust dtype as necessary
+    def to_ints(df):
+        df['Last_Update'] = pd.to_datetime(df['Last_Update']).dt.date
+        df['Last_Update'] = pd.to_datetime(df['Last_Update'])
+        df.rename(columns={'Last_Update': 'Date'}, inplace=True)
+        df['Confirmed'] = df['Confirmed'].astype("int")
+        df['Deaths'] = df['Deaths'].astype("int")
+        # df['People_Tested'].str.replace('.', '').astype(int)
+        df['People_Tested'] = pd.to_numeric(df['People_Tested'])
+        # df['People_Tested'] = df['People_Tested'].astype("int")
+        df['People_Hospitalized'] = pd.to_numeric(df['People_Hospitalized'])
+        # df['People_Hospitalized'] = df['People_Hospitalized'].astype("int")
+        return df
 
 
-def daily_data(df):
-    df['Daily_Cases'] = df['Confirmed'] - df['Prev_Cases']
-    df['Daily_Deaths'] = df['Deaths'] - df['Prev_Deaths']
-    df['Daily_Tests'] = df['People_Tested'] - df['Prev_Tests']
-    df['Daily_Hospitalizations'] = df['People_Hospitalized'] - df['Prev_Hospitalizations']
-    del df['Prev_Hospitalizations'], df['Prev_Deaths'], df['Prev_Cases'], df['Prev_Tests']
-    df = df[(df['Date'] > "2020-04-13")]
-    return df
+    def shift_data(df):
+        df['Prev_Cases'] = df.groupby("Province_State")["Confirmed"].shift(1)
+        df['Prev_Deaths'] = df.groupby("Province_State")["Deaths"].shift(1)
+        df['Prev_Tests'] = df.groupby("Province_State")["People_Tested"].shift(1)
+        df['Prev_Hospitalizations'] = df.groupby("Province_State")["People_Hospitalized"].shift(1)
+        df = df.fillna(0)
+        return df
 
 
-def kpis(df):
-    df['Positivity_Rate'] = df['Daily_Cases'] / df['Daily_Tests']
-    return df
+    def daily_data(df):
+        df['Daily_Cases'] = df['Confirmed'] - df['Prev_Cases']
+        df['Daily_Deaths'] = df['Deaths'] - df['Prev_Deaths']
+        df['Daily_Tests'] = df['People_Tested'] - df['Prev_Tests']
+        df['Daily_Hospitalizations'] = df['People_Hospitalized'] - df['Prev_Hospitalizations']
+        del df['Prev_Hospitalizations'], df['Prev_Deaths'], df['Prev_Cases'], df['Prev_Tests']
+        df = df[(df['Date'] > "2020-04-13")]
+        return df
 
 
-def moving_avg(df):
-    df['14_Day_Avg_Positivity_Rate'] = df.groupby('Province_State')['Positivity_Rate'].transform(
-        lambda x: x.rolling(14, 1).mean())
-    df['14_Day_Avg_Tests'] = df.groupby('Province_State')['Daily_Tests'].transform(
-        lambda x: x.rolling(14, 1).mean())
-    return df
+    def kpis(df):
+        df['Positivity_Rate'] = df['Daily_Cases'] / df['Daily_Tests']
+        return df
 
 
-covid_data = to_ints(covid_data)
-covid_data = shift_data(covid_data)
-covid_data = daily_data(covid_data)
-covid_data = kpis(covid_data)
-covid_data = moving_avg(covid_data)
+    def moving_avg(df):
+        df['14_Day_Avg_Positivity_Rate'] = df.groupby('Province_State')['Positivity_Rate'].transform(
+            lambda x: x.rolling(14, 1).mean())
+        df['14_Day_Avg_Tests'] = df.groupby('Province_State')['Daily_Tests'].transform(
+            lambda x: x.rolling(14, 1).mean())
+        return df
+
+
+covid_data = COVID.to_ints(covid_data)
+covid_data = COVID.shift_data(covid_data)
+covid_data = COVID.daily_data(covid_data)
+covid_data = COVID.kpis(covid_data)
+covid_data = COVID.moving_avg(covid_data)
 
 
 states = ["New York", "California", "Texas", "Alabama", "North Carolina", "South Carolina", "Michigan",
